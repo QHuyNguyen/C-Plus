d61c626daf6a4bb4313c3f968d684bb9ef2441f0
Quoc Huy Nguyen <quochuynguyen210@gmail.com>
2020-03-08 22:27:50 +1100

    Add Constructor Folder

---
+++ Constructor/Constructor/main.cpp
@@ -0,0 +1,37 @@
+//
+//  main.cpp
+//  Constructor
+//
+//  Created by Quoc Huy Nguyen on 8/3/20.
+//  Copyright © 2020 Quoc Huy Nguyen. All rights reserved.
+//
+
+#include <iostream>
+
+class Square{
+public:
+    int X, Y;
+    Square(){
+        X = 3;
+        Y = 3;
+    }
+    Square(int x, int y){
+        X = x;
+        Y = y;
+    }
+public:
+    void Print(){
+        std::cout << "Square area is " << X*Y << std::endl;
+    };
+};
+
+int main(int argc, const char * argv[]) {
+    std::cout << "Enter square length: ";
+    int size;
+    std::cin >> size;
+    
+    Square square(size, size);
+    square.Print();
+    
+    return 0;
+}
